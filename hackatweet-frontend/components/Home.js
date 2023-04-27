107af525ba29b6e649911d20141324b5139507ee
Hugo <hugolevisse1@gmail.com>
2023-04-27 16:01:56 +0200

    Home page

--- hackatweet-frontend/components/Home.js
+++ hackatweet-frontend/components/Home.js
@@ -1,12 +1,24 @@
 import styles from '../styles/Home.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import Link from 'next/link';
+
 
 function Home() {
+
   return (
     <div>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className={styles.left}>
+        <FontAwesomeIcon className={styles.logoHome} icon={faTwitter} size="lg" rotation={180} style={{color: "#ffffff",}} />
+        </div>
+        <div className={styles.right}>
+          <h2 className={styles.bigTitle}>See what's <br/> happening</h2>
+          <h4>Join Hackatweet today.</h4>
+          <button className={styles.signUpButton}>Sign Up</button>
+          <h6>Already have an account?</h6>
+          <button className={styles.signInButton}>Sign In</button>
+        </div>
       </main>
     </div>
   );
